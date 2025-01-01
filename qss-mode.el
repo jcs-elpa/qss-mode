464a2e86e30d20e773ecb96dfd747851de619ee1
JenChieh <jcs090218@gmail.com>
2025-01-01 02:11:41 -0800

    Update copyright year

--- qss-mode.el
+++ qss-mode.el
@@ -1,6 +1,6 @@
 ;;; qss-mode.el --- Major mode for Qt Style Sheets  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023-2024  Shen, Jen-Chieh
+;; Copyright (C) 2023-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
